e23370572847b8d77082f87d4e55e77762c76858
Chimene-Druiventak <70636601+Chimene-Druiventak@users.noreply.github.com>
2021-02-16 17:41:08 +0100

    Update knikkerbaan.ino

--- knikkerbaan/knikkerbaan.ino
+++ knikkerbaan/knikkerbaan.ino
@@ -1,37 +1,62 @@
-const int ledPin = 13;
-
-const int ldrPin = A0;
-
+/* 
+  IR Breakbeam sensor demo!
+*/
+#include <Servo.h>
+#define LEDPIN 13
+  // Pin 13: Arduino has an LED connected on pin 13
+  // Pin 11: Teensy 2.0 has the LED on pin 11
+  // Pin  6: Teensy++ 2.0 has the LED on pin 6
+  // Pin 13: Teensy 3.0 has the LED on pin 13
+ 
+#define SENSORPIN 4
+ 
+// variables will change:
+Servo myservo; 
+int pos = 0;  
+int sensorState = 0, lastState=0;         // variable for reading the pushbutton status
+ 
 void setup() {
+  // initialize the LED pin as an output:
+  pinMode(LEDPIN, OUTPUT);      
+  // initialize the sensor pin as an input:
+  pinMode(SENSORPIN, INPUT);     
+  digitalWrite(SENSORPIN, HIGH); // turn on the pullup
+  myservo.attach(9);
   
   Serial.begin(9600);
-
-  pinMode(ledPin, OUTPUT);
-
-  pinMode(ldrPin, INPUT);
-
-
 }
-
-void loop() {
-  int ldrStatus = analogRead(ldrPin);
-
-  if (ldrStatus <= 100) {
-
-    digitalWrite(ledPin, HIGH);
-
-    Serial.print("Its DARK, Turn on the LED : ");
-
-    Serial.println(ldrStatus);
-
+ 
+void loop(){
+  // read the state of the pushbutton value:
+  sensorState = digitalRead(SENSORPIN);
+ 
+  // check if the sensor beam is broken
+  // if it is, the sensorState is LOW:
+  if (sensorState == LOW) {     
+    // turn LED on:
+    digitalWrite(LEDPIN, HIGH);  
   } 
   else {
-
-    digitalWrite(ledPin, LOW);
-
-    Serial.print("Its BRIGHT, Turn off the LED : ");
-
-    Serial.println(ldrStatus);
-
+    // turn LED off:
+    digitalWrite(LEDPIN, LOW); 
   }
+  
+  if (sensorState && !lastState) {
+    Serial.println("Unbroken");
+
+    } 
+    
+  if (!sensorState && lastState) {
+    Serial.println("Broken");
+      for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+                       // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(10); 
+      }
+      for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(10);                       // waits 15ms for the servo to reach the position
+    }
+  }
+  lastState = sensorState;
 }

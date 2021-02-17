50c6e80ea98b099fa30009b3733e292af269bdfc
ElineRomijn <70636222+ElineRomijn@users.noreply.github.com>
2021-02-17 22:25:46 +0100

    Update knikkerbaan.ino
    code ledstrip op de ir sensor 17-2. morgen nog even in tinkercad laatste keer checken

--- knikkerbaan/knikkerbaan.ino
+++ knikkerbaan/knikkerbaan.ino
@@ -60,3 +60,97 @@ void loop(){
   }
   lastState = sensorState;
 }
+
+
+==========================================BIJNA GOEDE CODE LEDSTRIP============================================================
+  #include <Adafruit_NeoPixel.h>
+
+#define PIN 2	 // input pin Neopixel is attached to
+
+#define NUMPIXELS      12 // number of neopixels in strip
+
+#include <Servo.h>
+
+#define SENSORPIN 4
+ 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 100; // timing delay in milliseconds
+
+int redColor = 0;
+int greenColor = 0;
+int blueColor = 0;
+// variables will change:
+Servo myservo; 
+int pos = 0;  
+int sensorState = 0, lastState=0;
+
+void setup() {
+  // Initialize the NeoPixel library.
+  pixels.begin();
+  // initialize the LED pin as an output:
+  pinMode(LEDPIN, OUTPUT);      
+  // initialize the sensor pin as an input:
+  pinMode(SENSORPIN, INPUT);     
+  digitalWrite(SENSORPIN, HIGH); // turn on the pullup
+  myservo.attach(9);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  sensorState = digitalRead(SENSORPIN);
+ 
+  // check if the sensor beam is broken
+  // if it is, the sensorState is LOW:
+  if (sensorState == LOW) {     
+    // turn LED on:
+    setColor();
+  
+  for (int i=0; i < NUMPIXELS; i++) {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    
+    pixels.setPixelColor(i, pixels.Color(redColor, greenColor, blueColor));
+    
+    // This sends the updated pixel color to the hardware.
+    pixels.show();
+
+    // Delay for a period of time (in milliseconds).
+    delay(delayval);
+    }  
+  } 
+  /*else {
+    // turn LED off:
+    digitalWrite(LEDPIN, LOW); 
+  }*/
+  
+  if (sensorState && !lastState) {
+    Serial.println("Unbroken");
+
+    } 
+    
+  if (!sensorState && lastState) {
+    Serial.println("Broken");
+      for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+                       // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(10); 
+      }
+      for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(10);                       // waits 15ms for the servo to reach the position
+    }
+  }
+  lastState = sensorState;
+}
+  
+   
+
+// setColor()
+// picks random values to set for RGB
+void setColor(){
+  redColor = random(0, 255);
+  greenColor = random(0,255);
+  blueColor = random(0, 255);
+}

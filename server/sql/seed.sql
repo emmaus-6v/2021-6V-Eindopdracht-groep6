c97bf280d2bf642a203831b0a093543ddfd64713
ElineRomijn <70636222+ElineRomijn@users.noreply.github.com>
2021-02-27 23:04:57 +0100

    ?

--- server/sql/seed.sql
+++ server/sql/seed.sql
@@ -1,5 +1,5 @@
 /*INSERT INTO baanStatus (status, tijd, opmerking) VALUES (1, CURRENT_TIMESTAMP, "Server is aan")
 INSERT INTO buttonPresses (tijd) VALUES (CURRENT_TIMESTAMP);*/
 
-INSERT INTO sensorstatus1 (status, tijd, opmerking) VALUES (HIGH, CURRENT_TIMESTAMP, "Server is aan");
-INSERT INTO sensorstatus2 (status, tijd, opmerking) VALUES (HIGH, CURRENT_TIMESTAMP, "Server is aan");
+INSERT INTO sensorstate (status, tijd, opmerking) VALUES (HIGH, CURRENT_TIMESTAMP, "Server is aan");
+INSERT INTO sensorstate2 (status, tijd, opmerking) VALUES (HIGH, CURRENT_TIMESTAMP, "Server is aan");

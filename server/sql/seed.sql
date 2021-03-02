28504da6d22570a2df3c28e1ddcff5f5796004d1
ElineRomijn <70636222+ElineRomijn@users.noreply.github.com>
2021-03-02 11:19:20 +0100

    totaalknikkers toegevoegd

--- server/sql/seed.sql
+++ server/sql/seed.sql
@@ -3,3 +3,4 @@ INSERT INTO buttonPresses (tijd) VALUES (CURRENT_TIMESTAMP);*/
 
 INSERT INTO sensorstate (status, tijd, opmerking) VALUES (1, CURRENT_TIMESTAMP, "Server is aan");
 INSERT INTO sensorstate2 (status, tijd, opmerking) VALUES (1, CURRENT_TIMESTAMP, "Server is aan");
+INSERT INTO totaalKnikkers (totaal, tijd, opmerking) VALUES (0, CURRENT_TIMESTAMP, "Server is aan");

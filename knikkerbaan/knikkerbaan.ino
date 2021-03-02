0af8e3e76f7eb41c8d214361702fdd5a8cc7b84e
Chimene-Druiventak <70636601+Chimene-Druiventak@users.noreply.github.com>
2021-03-02 21:37:06 +0100

    Update knikkerbaan.ino

--- knikkerbaan/knikkerbaan.ino
+++ knikkerbaan/knikkerbaan.ino
@@ -1,66 +1,3 @@
-/* 
-  IR Breakbeam sensor demo!
-*/
-#include <Servo.h>
-#define LEDPIN 13
-  // Pin 13: Arduino has an LED connected on pin 13
-  // Pin 11: Teensy 2.0 has the LED on pin 11
-  // Pin  6: Teensy++ 2.0 has the LED on pin 6
-  // Pin 13: Teensy 3.0 has the LED on pin 13
- 
-#define SENSORPIN 4
- 
-// variables will change:
-Servo myservo; 
-int pos = 0;  
-int sensorState = 0, lastState=0;         // variable for reading the pushbutton status
- 
-void setup() {
-  // initialize the LED pin as an output:
-  pinMode(LEDPIN, OUTPUT);      
-  // initialize the sensor pin as an input:
-  pinMode(SENSORPIN, INPUT);     
-  digitalWrite(SENSORPIN, HIGH); // turn on the pullup
-  myservo.attach(9);
-  
-  Serial.begin(9600);
-}
- 
-void loop(){
-  // read the state of the pushbutton value:
-  sensorState = digitalRead(SENSORPIN);
- 
-  // check if the sensor beam is broken
-  // if it is, the sensorState is LOW:
-  if (sensorState == LOW) {     
-    // turn LED on:
-    digitalWrite(LEDPIN, HIGH);  
-  } 
-  else {
-    // turn LED off:
-    digitalWrite(LEDPIN, LOW); 
-  }
-  
-  if (sensorState && !lastState) {
-    Serial.println("Unbroken");
-
-    } 
-    
-  if (!sensorState && lastState) {
-    Serial.println("Broken");
-      for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
-                       // in steps of 1 degree
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(10); 
-      }
-      for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-        delay(10);                       // waits 15ms for the servo to reach the position
-    }
-  }
-  lastState = sensorState;
-}
-
 //==========================================================================================================================
 //==========================================code 2 sensors bij elkaar============================================================
 //==========================================================================================================================
@@ -274,7 +211,8 @@ void setup() {
 
   //-----------setup voor led, servo, ledstrip en sensors -------------------------
   // Initialize the NeoPixel library.
-  pixels.begin();     
+  pixels.begin();
+  pixels.set Brightness(64);
 
   // initialize the LED pin as an output:
   pinMode(LEDPIN, OUTPUT); 
@@ -350,11 +288,11 @@ void loop() {
   //----------motor anticiperen op sensor1 waarde ---------------------
   if (!sensorState && lastState) {
     Serial.println("Broken");
-      for (pos = 0; pos <= 90; pos += 1) {                  
+      for (pos = 0; pos <= 55; pos += 1) {                  
       myservo.write(pos);              
       delay(10); 
       }
-      for (pos = 90; pos >= 0; pos -= 1) { 
+      for (pos = 55; pos >= 0; pos -= 1) { 
       myservo.write(pos);             
         delay(10);                       
     }
